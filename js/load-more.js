7dbbd8df50786332d2a6ff786a18ac05f3fb1e24
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-03-09 16:59:35 +0200

    Update load-more.js

--- js/load-more.js
+++ js/load-more.js
@@ -1,20 +1,3 @@
-// let loadMoreBtn = document.querySelector("#load-more");
-// let currentItem = 3;
-
-// loadMoreBtn.onclick = () => {
-//   let boxes = [
-//     ...document.querySelectorAll(".projects .projects__container .projects__item"),];
-
-//   for (let i = currentItem; i < currentItem + 3; i++) {
-//     boxes[i].style.display = "block";
-//   }
-//   currentItem += 3;
-
-//   if (currentItem >= boxes.length) {
-//     loadMoreBtn.style.display = "none";
-//   }
-// };
-
 const loadMoreButton = document.getElementById("load-more");
 const projectItems = document.querySelectorAll(".projects__item");
 let visibleItems = 3; // Начальное количество видимых элементов
