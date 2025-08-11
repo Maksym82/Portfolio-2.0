e1223c21bf27a9518505b6fdf5844aa2ecfafda2
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-08-11 12:19:08 +0300

    Fix errors

--- js/faq.js
+++ js/faq.js
@@ -1,16 +1,3 @@
-// const items = document.querySelectorAll(".accordion__item");
-
-// items.forEach((item) => {
-//   const button = item.querySelector(".accordion__button");
-//   button.addEventListener("click", () => {
-//     const isActive = item.classList.contains("active");
-//     items.forEach((i) => i.classList.remove("active"));
-//     if (!isActive) {
-//       item.classList.add("active");
-//     }
-//   });
-// });
-
 const headers = document.querySelectorAll('.accordion__button');
 
     headers.forEach(header => {
